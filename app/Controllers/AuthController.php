20eb4b1a3fc1dcae90cc54c3845c9e2ad3a064ec
Juu290606 <juanpratama339@gmail.com>
2026-05-17 14:32:18 +0700

    menambahkan migration dan seeding

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends BaseController
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');
 
-        $dataUser = ['username' => 'Juan', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
+        $dataUser = ['username' => 'J`uan', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
 
         if ($username == $dataUser['username']) {
             if (md5($password) == $dataUser['password']) {
